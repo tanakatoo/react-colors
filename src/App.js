1cd04c70831dc22e0f8b5210efe25de4adc8314c
tanakatoo <karmen.tanaka@gmail.com>
2023-05-05 09:44:14 -0400

    done

--- src/App.js
+++ src/App.js
@@ -1,35 +1,30 @@
 
 import './App.css';
-import Nav from "./Nav"
-import Form from "./Form"
 import Routes from './Routes';
-import { Redirect } from "react-router-dom"
+import { Redirect, useHistory } from "react-router-dom"
 import { useEffect, useState } from 'react';
-import Home from "./Home"
+
 
 function App() {
+  const history = useHistory()
   const INITIAL_STATE = [
     { name: 'Red', hex: '#880808' },
     { name: 'Blue', hex: '#F0FFFF' },
     { name: 'Yellow', hex: '#FFBF00' }]
   const [colors, setColors] = useState(INITIAL_STATE)
-  const [goHome, setGoHome] = useState(false)
 
   const addData = ({ name, hex }) => {
     setColors(colors => [{ name, hex }, ...colors])
     console.log('in setting colors', colors)
+    return <Redirect to="/colors" />
   }
 
   useEffect(() => {
-    setGoHome(true)
-    console.log('go home is true, so go home')
-      < Redirect to = "/colors" />
-    return () => setGoHome(false)
+    history.push('/colors')
   }, [colors])
 
   return (
     <div className="App">
-      {goHome ? <Redirect to="/colors" /> : ''}
       <Routes colors={colors} addData={addData} />
     </div>
   );
